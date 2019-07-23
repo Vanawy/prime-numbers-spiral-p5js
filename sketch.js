49a59a05bb3183ef7aa83767d0a7667fdbbdd4f1
Ilya Sakharchuk <saharchuk999@gmail.com>
2019-07-23 21:39:23 +0300

    upd

--- sketch.js
+++ sketch.js
@@ -1,22 +1,38 @@
 let i = 0;
 let count = 1;
-let pos, direction;
+let x, y, dx, dy, maxI, t, X, Y;
 
 function setup() {
   createCanvas(400, 400);
   background(220);
-  pos = createVector(width/2, height/2);
-  direction = createVector(1, 0);
+  X = width;
+  Y = height;
+  x = y = dx = 0;
+  dy = -1;
+  t = Math.max(X/3, Y/3);
+  maxI = t * t;
+  stroke(0);
 }
 
 function draw() {
-  if(isPrime(i)){
-    stroke("red");  
-  }else{
-    stroke(0);
+  if(i < maxI){
+    if ((-X / 2 <= x) && (x <= X / 2) && (-Y / 2 <= y) && (y <= Y / 2)) {
+      if(isPrime(i)){
+        fill(0, 255, 255);  
+      }else{
+        fill(0);
+      }
+      rect(x*3 + X/2, y*3 + Y/2, 3, 3);
+    }
+    if ((x == y) || ((x < 0) && (x == -y)) || ((x > 0) && (x == 1 - y))) {
+      t = dx;
+      dx = -dy;
+      dy = t;
+    }
+    x += dx;
+    y += dy;
+    i++;
   }
-  point(i % width, Math.floor(i / width));
-  i++;
 }
 
 const isPrime = num => {
